3089c748886f1ed29969a0c6e320ad2cc726c327
赖骏劼 <laijj@asiainfo.com>
2021-10-30 11:08:43 +0800

    @bpascal@集成PermissionX测试

--- demo/src/main/java/com/codido/nymeria/demo/MainActivity.kt
+++ demo/src/main/java/com/codido/nymeria/demo/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.codido.nymeria.demo
 
+import android.Manifest
+import android.widget.Toast
 import com.codido.nymeria.NymeriaBaseActivity
 import com.codido.nymeria.demo.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
+import com.permissionx.guolindev.PermissionX
 
 /**
  * 主入口
@@ -23,8 +26,9 @@ class MainActivity : NymeriaBaseActivity<ActivityMainBinding>() {
     override fun initEvents() {
         super.initEvents()
         _viewBinding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+            requestPermissionX();
         }
     }
 
@@ -33,6 +37,29 @@ class MainActivity : NymeriaBaseActivity<ActivityMainBinding>() {
      */
     override fun initDatas() {
         super.initDatas()
+    }
+
+    /**
+     * 申请权限方法
+     */
+    fun requestPermissionX() {
+        PermissionX.init(this)
+            .permissions(
+                Manifest.permission.READ_CONTACTS,
+                Manifest.permission.CAMERA,
+                Manifest.permission.CALL_PHONE
+            )
+            .request { allGranted, grantedList, deniedList ->
+                if (allGranted) {
+                    Toast.makeText(this, "All permissions are granted", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "These permissions are denied: $deniedList",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
     }
 
     override lateinit var _viewBinding: ActivityMainBinding
